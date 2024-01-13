9a173740c3fb095eb9c670fb5330d7ac51963de6
xjm <alibi_jia@foxmail.com>
2024-01-13 15:14:49 +0800

    根据issues2188改回

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>fresh's hello world</title>
-        <link rel="stylesheet" href={asset("/styles.css")} />
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <header class="px-4 py-8 mx-auto bg-[#86efac]">
