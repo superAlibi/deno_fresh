229ec2bea01dc99b57c29003112a18379fdb15cf
worker-xjm <worker_xjm@outlook.com>
2024-06-19 10:51:42 +0800

    新增我的sb php nt同事的接口模拟

--- routes/api/usercenter/login.account.ts
+++ routes/api/usercenter/login.account.ts
@@ -2,6 +2,7 @@ import { Handler } from "$fresh/server.ts";
 
 export const handler: Handler = (req) => {
  
+  console.log(req.url);
   
   return new Response(req.method);
 };
--- routes/usercenter/login.account.ts
+++ routes/usercenter/login.account.ts
@@ -1,6 +1,6 @@
 import { Handler } from "$fresh/server.ts";
 
 export const handler: Handler = (req) => {
-  console.log('已受到请求');
+  console.log('已受到请求', req.url);
   return new Response("已受到请求",{status:500});
 };
