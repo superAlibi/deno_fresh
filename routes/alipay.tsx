5d82b5858266c118030fbfcc5b69a01fdbf99924
worker-xjm <worker_xjm@outlook.com>
2024-06-25 14:30:56 +0800

    调整location取值位置

--- routes/alipay.tsx
+++ routes/alipay.tsx
@@ -3,9 +3,9 @@ import { Handlers } from "$fresh/server.ts";
 export const handler: Handlers = {
   GET() {
     const appid = Deno.env.get("ALIPAY_APPID");
-    // /api/alipay/redirect.ts
-    console.log(location?.host);
-    const urlobj = new URL(location?.href);
+    const location = Deno.env.get("location")!;
+    
+    const urlobj = new URL(location);
     urlobj.pathname = "/api/alipay/redirect";
     return new Response("", {
       status: 302,
