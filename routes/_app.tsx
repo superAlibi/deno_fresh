76b16fa5f419e21574fc3b0b0431eb1286a37abe
xjm <alibi_jia@foxmail.com>
2024-06-26 23:35:53 +0800

    主题色调整

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,7 +9,7 @@ export default defineApp((_, { Component }: PageProps) => {
         <title>家铭的个人网站</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body class="bg-[#86efac] h-screen">
+      <body class="bg-[#86efac] dark:bg-gray-950 text-lime-500 h-screen">
         <section  class="content p-8">
           <Component></Component>
         </section>
