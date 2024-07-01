a96500703af335f01f36a2b689272ce2bfc5b339
worker-xjm <worker_xjm@outlook.com>
2024-07-01 14:18:39 +0800

    调整authing初始化方式

--- tools/sdk/authing.ts
+++ tools/sdk/authing.ts
@@ -1,10 +1,8 @@
 import {AuthenticationClient} from 'authing'
-const redirect_uri = new URL(Deno.env.get("location")!);
-    redirect_uri.pathname = "/api/authing/parsecode";
-    
+
 export const resumeAuhtingSDK = new AuthenticationClient({
   appId: Deno.env.get('AUTHING_APP_ID')!,
   appHost: 'https://worker-xjm-resume.authing.cn',
   secret: Deno.env.get('AUTHING_APP_SECRET')!,
-  redirectUri: redirect_uri.toString(),
+  redirectUri: Deno.env.get("location")+'"/api/authing/parsecode"',
 })
