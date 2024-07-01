38bb2e9cdeecc0e8dba68e61cee314ee6ca822b9
worker-xjm <worker_xjm@outlook.com>
2024-07-01 14:13:28 +0800

    使用sdk方式拿到access_token

--- routes/api/authing/parsecode.ts
+++ routes/api/authing/parsecode.ts
@@ -1,6 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
+import { resumeAuhtingSDK } from "../../../tools/sdk/authing.ts";
 import { ParsedCTX } from "../../../types.d.ts";
-// import { resumeAuhtingSDK } from "../../tools/sdk/authing.ts";
 
 export const handler: Handlers<ParsedCTX> = {
   GET(req, ctx) {
@@ -14,28 +14,8 @@ export const handler: Handlers<ParsedCTX> = {
     }
     const state = ctx.data.query.get("state");
     // 认证参数拼装
-    const urlObj = new URL("https://worker-xjm-resume.authing.cn/oidc/token");
-    const { searchParams } = urlObj;
-    searchParams.append("code", code);
-    searchParams.append("client_id", Deno.env.get("AUTHING_APP_ID")!);
-    searchParams.append("client_secret", Deno.env.get("AUTHING_SECRET_KEY")!);
-    searchParams.append("grant_type", "authorization_code");
-    // 重定向地址
-    const redirect_uri = new URL(Deno.env.get("location")!);
-    redirect_uri.pathname = "/api/authing/parsecode";
-    searchParams.append("redirect_uri", redirect_uri.toString());
-    console.log(urlObj.toString());
-
-    fetch(urlObj, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }).then((RESP) => {
-      return RESP.json();
-    }).then((d) => {
-      console.log(JSON.stringify(d));
-    });
+    const result = resumeAuhtingSDK.getAccessTokenByCode(code);
+    console.log(result);
     return new Response(JSON.stringify(ctx.data.query.toString()));
   },
   POST(req, ctx) {
